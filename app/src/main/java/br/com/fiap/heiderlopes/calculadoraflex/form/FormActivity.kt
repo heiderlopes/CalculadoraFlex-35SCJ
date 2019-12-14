2bef63f0f8c94b65bf21f64e977b493dde384c26
Logon PF Fiap <logonpflocal@fiap.com.br>
2019-12-14 12:50:18 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/heiderlopes/calculadoraflex/form/FormActivity.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.heiderlopes.calculadoraflex.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.heiderlopes.calculadoraflex.R
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
---
+++ app/src/main/java/br/com/fiap/heiderlopes/calculadoraflex/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.heiderlopes.calculadoraflex.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.heiderlopes.calculadoraflex.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
